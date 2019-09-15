91fb72798de604b30432600f469e02286d785dea
nelsonr <robert.nelson@sys-service.de>
2019-09-15 17:56:13 +0200

    Add project files.

---
+++ AudioPlayerUsingWMPNeu/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace WindowsMediaPlayerTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.buttonPause = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.trackBarPosition = new System.Windows.Forms.TrackBar();
+            this.buttonTest = new System.Windows.Forms.Button();
+            this.trackBarVolume = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBoxFiles = new System.Windows.Forms.ListBox();
+            this.timerDuration = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelDuration = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabelPosition = new System.Windows.Forms.ToolStripStatusLabel();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarPosition)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarVolume)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonPlay.Location = new System.Drawing.Point(12, 460);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(47, 23);
+            this.buttonPlay.TabIndex = 2;
+            this.buttonPlay.Text = "Start";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // buttonPause
+            // 
+            this.buttonPause.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonPause.Location = new System.Drawing.Point(69, 460);
+            this.buttonPause.Name = "buttonPause";
+            this.buttonPause.Size = new System.Drawing.Size(58, 23);
+            this.buttonPause.TabIndex = 7;
+            this.buttonPause.Text = "Pause";
+            this.buttonPause.UseVisualStyleBackColor = true;
+            this.buttonPause.Click += new System.EventHandler(this.buttonPause_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonStop.Location = new System.Drawing.Point(133, 460);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(47, 23);
+            this.buttonStop.TabIndex = 8;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // trackBarPosition
+            // 
+            this.trackBarPosition.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.trackBarPosition.AutoSize = false;
+            this.trackBarPosition.Location = new System.Drawing.Point(13, 419);
+            this.trackBarPosition.Name = "trackBarPosition";
+            this.trackBarPosition.Size = new System.Drawing.Size(301, 20);
+            this.trackBarPosition.TabIndex = 9;
+            this.trackBarPosition.Scroll += new System.EventHandler(this.trackBarPosition_Scroll);
+            // 
+            // buttonTest
+            // 
+            this.buttonTest.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonTest.Location = new System.Drawing.Point(186, 460);
+            this.buttonTest.Name = "buttonTest";
+            this.buttonTest.Size = new System.Drawing.Size(47, 23);
+            this.buttonTest.TabIndex = 10;
+            this.buttonTest.Text = "Test";
+            this.buttonTest.UseVisualStyleBackColor = true;
+            this.buttonTest.Click += new System.EventHandler(this.buttonTest_Click);
+            // 
+            // trackBarVolume
+            // 
+            this.trackBarVolume.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.trackBarVolume.AutoSize = false;
+            this.trackBarVolume.Location = new System.Drawing.Point(13, 369);
+            this.trackBarVolume.Maximum = 100;
+            this.trackBarVolume.Name = "trackBarVolume";
+            this.trackBarVolume.Size = new System.Drawing.Size(301, 20);
+            this.trackBarVolume.TabIndex = 11;
+            this.trackBarVolume.Value = 20;
+            this.trackBarVolume.Scroll += new System.EventHandler(this.trackBarVolume_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 353);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Volume";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 403);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Position";
+            // 
+            // listBoxFiles
+            // 
+            this.listBoxFiles.AllowDrop = true;
+            this.listBoxFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBoxFiles.FormattingEnabled = true;
+            this.listBoxFiles.Location = new System.Drawing.Point(15, 249);
+            this.listBoxFiles.Name = "listBoxFiles";
+            this.listBoxFiles.Size = new System.Drawing.Size(298, 95);
+            this.listBoxFiles.TabIndex = 14;
+            this.listBoxFiles.Click += new System.EventHandler(this.listBoxFiles_Click);
+            this.listBoxFiles.SelectedIndexChanged += new System.EventHandler(this.listBoxFiles_SelectedIndexChanged);
+            this.listBoxFiles.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBoxFiles_DragDrop);
+            this.listBoxFiles.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBoxFiles_DragEnter);
+            // 
+            // timerDuration
+            // 
+            this.timerDuration.Interval = 1000;
+            this.timerDuration.Tick += new System.EventHandler(this.timerDuration_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelDuration,
+            this.toolStripStatusLabelPosition});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 489);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(326, 22);
+            this.statusStrip1.TabIndex = 15;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabelDuration
+            // 
+            this.toolStripStatusLabelDuration.Name = "toolStripStatusLabelDuration";
+            this.toolStripStatusLabelDuration.Size = new System.Drawing.Size(13, 17);
+            this.toolStripStatusLabelDuration.Text = "0";
+            // 
+            // toolStripStatusLabelPosition
+            // 
+            this.toolStripStatusLabelPosition.Name = "toolStripStatusLabelPosition";
+            this.toolStripStatusLabelPosition.Size = new System.Drawing.Size(13, 17);
+            this.toolStripStatusLabelPosition.Text = "0";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(301, 229);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            this.axWindowsMediaPlayer1.OpenStateChange += new AxWMPLib._WMPOCXEvents_OpenStateChangeEventHandler(this.axWindowsMediaPlayer1_OpenStateChange);
+            this.axWindowsMediaPlayer1.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.axWindowsMediaPlayer1_PlayStateChange);
+            this.axWindowsMediaPlayer1.StatusChange += new System.EventHandler(this.axWindowsMediaPlayer1_StatusChange);
+            this.axWindowsMediaPlayer1.EndOfStream += new AxWMPLib._WMPOCXEvents_EndOfStreamEventHandler(this.axWindowsMediaPlayer1_EndOfStream);
+            this.axWindowsMediaPlayer1.PositionChange += new AxWMPLib._WMPOCXEvents_PositionChangeEventHandler(this.axWindowsMediaPlayer1_PositionChange);
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 511);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listBoxFiles);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBarVolume);
+            this.Controls.Add(this.buttonTest);
+            this.Controls.Add(this.trackBarPosition);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonPause);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.MinimumSize = new System.Drawing.Size(342, 550);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarPosition)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarVolume)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button buttonPause;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.TrackBar trackBarPosition;
+        private System.Windows.Forms.Button buttonTest;
+        private System.Windows.Forms.TrackBar trackBarVolume;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBoxFiles;
+        private System.Windows.Forms.Timer timerDuration;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelDuration;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelPosition;
+    }
+}
+
---
+++ AudioPlayerUsingWMPNeu/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace WindowsMediaPlayerTest
+{
+    public partial class Form1 : Form
+    {
+        double _duration = 0;
+        double _position = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "WindowsMediaPlayer2";
+            axWindowsMediaPlayer1.settings.volume = 10;
+        }
+
+        private void listBoxFiles_DragEnter(object sender, DragEventArgs e)
+        {
+            Debug.Print("listBoxFiles_DragEnter");
+
+            if(e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void listBoxFiles_DragDrop(object sender, DragEventArgs e)
+        {
+            Debug.Print("listBoxFiles_DragDrop");
+            if(e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] filenames = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+                listBoxFiles.Items.Clear();
+
+                foreach(string s in filenames)
+                {
+                    listBoxFiles.Items.Add(s);
+                }
+
+                listBoxFiles.SelectedIndex = 0;
+            }
+        }
+
+        private void listBoxFiles_Click(object sender, EventArgs e)
+        {
+            Debug.Print("listBoxFiles_Click");
+            buttonPlay.PerformClick();
+        }
+
+        private void listBoxFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Debug.Print("listBoxFiles_SelectedIndexChanged");
+        }
+
+        private void trackBarVolume_Scroll(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.settings.volume = trackBarVolume.Value;
+            Debug.Print(axWindowsMediaPlayer1.settings.volume.ToString());
+        }
+
+        private void buttonPlay_Click(object sender, System.EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = listBoxFiles.Text;
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+            timerDuration.Enabled = true;
+        }
+
+        private void buttonPause_Click(object sender, System.EventArgs e)
+        {
+            if(buttonPause.Text == "Pause")
+            {
+                buttonPause.Text = "Resume";
+                axWindowsMediaPlayer1.Ctlcontrols.pause();
+                timerDuration.Enabled = false;
+            }
+            else
+            {
+                buttonPause.Text = "Pause";
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+                timerDuration.Enabled = true;
+            }
+        }
+
+        private void buttonStop_Click(object sender, System.EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+            timerDuration.Enabled = false;
+        }
+
+        private void buttonTest_Click(object sender, EventArgs e)
+        {
+            Debug.Print("buttonTest_Click");
+            listBoxFiles.SelectedIndex = listBoxFiles.SelectedIndex + 1;
+        }
+
+        private void trackBarPosition_Scroll(object sender, System.EventArgs e)
+        {
+            int intval = trackBarPosition.Value;
+            this.axWindowsMediaPlayer1.Ctlcontrols.currentPosition = intval;
+        }
+
+        private void axWindowsMediaPlayer1_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            Debug.Print($"PlayStateChange: newState={e.newState}");
+
+            string fileName = "";
+
+            if(e.newState == (int)WMPPlayState.wmppsStopped)
+            {
+                if(listBoxFiles.SelectedIndex < listBoxFiles.Items.Count - 1)
+                {
+                    listBoxFiles.SelectedIndex++;
+                    fileName = listBoxFiles.Text;
+                }
+                else
+                    fileName = "";
+
+                BeginInvoke(
+                    new Action(() => { axWindowsMediaPlayer1.URL = fileName; })
+                );
+            }
+
+            if(e.newState == (int)WMPPlayState.wmppsPlaying)
+            {
+                _duration = axWindowsMediaPlayer1.currentMedia.duration;
+                trackBarPosition.Maximum = (int)_duration;
+                toolStripStatusLabelDuration.Text = axWindowsMediaPlayer1.currentMedia.durationString;
+                Debug.Print(_duration.ToString());
+            }
+        }
+
+        private void axWindowsMediaPlayer1_OpenStateChange(object sender, AxWMPLib._WMPOCXEvents_OpenStateChangeEvent e)
+        {
+            Debug.Print("OpenStateChange");
+        }
+
+        private void axWindowsMediaPlayer1_EndOfStream(object sender, AxWMPLib._WMPOCXEvents_EndOfStreamEvent e)
+        {
+            Debug.Print("EndOfStream");
+        }
+
+        private void axWindowsMediaPlayer1_PositionChange(object sender, AxWMPLib._WMPOCXEvents_PositionChangeEvent e)
+        {
+            Debug.Print("PositionChange: oldposition=" + e.oldPosition);
+            Debug.Print("PositionChange: newposition=" + e.newPosition);
+            toolStripStatusLabelPosition.Text = axWindowsMediaPlayer1.Ctlcontrols.currentPositionString;
+        }
+
+        private void axWindowsMediaPlayer1_StatusChange(object sender, EventArgs e)
+        {
+            Debug.Print("StatusChange: " + e.ToString());
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+            Debug.Print($"WindowsMediaPlayer Enter: {e.ToString()}");
+        }
+
+        private void timerDuration_Tick(object sender, EventArgs e)
+        {
+            _position = axWindowsMediaPlayer1.Ctlcontrols.currentPosition;
+            trackBarPosition.Value = (int)_position;
+            toolStripStatusLabelPosition.Text = axWindowsMediaPlayer1.Ctlcontrols.currentPositionString;
+        }
+
+    }
+}
